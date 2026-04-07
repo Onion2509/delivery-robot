c635f2d5b29d0bc42862dcf47460cfbac4b2380b
onion <2697352379@qq.com>
2026-04-07 08:32:56 +0800

    add five test for pathplanner

--- rust_ws/libs/path_planning/src/lib.rs
+++ rust_ws/libs/path_planning/src/lib.rs
@@ -50,3 +50,100 @@ impl PathPlanner for AStartPlanner {
         Err(PathPlanningError::NoPathFound)
     }
 }
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+
+    #[test]
+    fn returns_start_out_of_bounds_when_start_is_invalid() {
+        let planner = AStartPlanner;
+        let map = GridMap::new(3,3);
+
+        assert_eq!(
+            planner.plan(
+                &map,
+                GridPosition{ x: 9, y: 0 },
+                GridPosition { x: 1, y: 1 },
+            ),
+            Err(PathPlanningError::StartOutOfBounds)
+        );
+    }
+
+    #[test]
+    fn returns_goal_out_of_bounds_when_goal_is_invalid() {
+        let planner = AStartPlanner;
+        let map = GridMap::new(3,3);
+
+        assert_eq!(
+            planner.plan(
+                &map,
+                GridPosition { x: 0, y: 0 },
+                GridPosition { x: 9, y: 9 },
+            ),
+            Err(PathPlanningError::GoalOutOfBounds)
+        );
+    }
+
+    #[test]
+    fn returns_start_blocked_when_start_cannot_be_walked() {
+        let planner = AStartPlanner;
+        let mut map = GridMap::new(3, 3);
+        
+        map.set_walkable(GridPosition { x: 0, y: 0 }, false).unwrap();
+
+        assert_eq!(
+            planner.plan(
+                &map,
+                GridPosition { x: 0, y: 0 },
+                GridPosition { x: 2, y: 2 },
+            ),
+            Err(PathPlanningError::StartBlocked)
+        );
+    }
+
+    #[test]
+    fn returns_goal_blocked_when_goal_cannot_be_walked() {
+        let planner = AStartPlanner;
+        let mut map = GridMap::new(3, 3);
+
+        map.set_walkable(GridPosition { x: 2, y: 2 }, false).unwrap();
+
+        assert_eq!(
+            planner.plan(
+                &map,
+                GridPosition { x: 0, y: 0 },
+                GridPosition { x: 2, y: 2 },
+            ),
+            Err(PathPlanningError::GoalBlocked)
+        );
+    }
+
+    #[test]
+    fn returns_single_point_path_when_start_equals_goal() {
+        let planner = AStartPlanner;
+        let map = GridMap::new(3, 3);
+        let point = GridPosition { x:1, y:1 };
+
+        assert_eq!(
+            planner.plan(&map, point, point),
+            Ok(vec![point])
+        );
+    }
+
+    #[test]
+    fn returns_no_path_found_for_different_points_in_stub_version() {
+        let planner = AStartPlanner;
+        let map = GridMap::new(3, 3);
+
+        assert_eq!(
+            planner.plan(
+                &map,
+                GridPosition { x: 0, y: 0 },
+                GridPosition { x: 2, y: 2 },
+            ),
+            Err(PathPlanningError::NoPathFound)
+        );
+    }
+}
