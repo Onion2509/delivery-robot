43ffecb8ce99622d09b45da5718414268807477c
onion <2697352379@qq.com>
2026-04-12 17:34:48 +0800

    print path in map

--- rust_ws/apps/planner_cli/src/main.rs
+++ rust_ws/apps/planner_cli/src/main.rs
@@ -1,6 +1,35 @@
 use grid_map::{GridMap, GridPosition};
 use path_planning::{AStartPlanner, PathPlanner};
 
+fn print_map_with_path(
+    map: &GridMap,
+    path: &[GridPosition],
+    start: GridPosition,
+    goal: GridPosition,
+) {
+    for y in 0..map.height() {
+        for x in 0..map.width() {
+            let position = GridPosition { x, y };
+
+            let symbol = if position == start {
+                'S'
+            } else if position == goal {
+                'G'
+            } else if path.contains(&position) {
+                '*'
+            } else if map.is_walkable(position).unwrap() {
+                '.'
+            } else {
+                '#'
+            };
+
+            print!("{}", symbol);
+        }
+
+        println!();
+    }
+}
+
 fn main() {
     let planner = AStartPlanner;
     let mut map = GridMap::new(3, 3);
@@ -12,7 +41,12 @@ fn main() {
     let goal = GridPosition { x: 2, y: 2 };
 
     match planner.plan(&map, start, goal) {
-        Ok(path) => println!("planned path: {:?}", path),
-        Err(error) => println!("planning failed: {:?}", error),
+        Ok(path) => {
+            println!("planned path: {:?}", path);
+            print_map_with_path(&map, &path, start, goal);
+        }
+        Err(error) => {
+            println!("planning failed: {:?}", error);
+        }
     }
 }
